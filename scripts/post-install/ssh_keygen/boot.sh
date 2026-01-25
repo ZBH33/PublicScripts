d24106a7d984a69542f0df6d405b01e17723f34f
ZBH33 <zbillman.work@gmail.com>
2026-01-26 09:47:03 +1030

    up for curl hopfully
    Signed-off-by: ZBH33 <zbillman.work@gmail.com>

--- scripts/post-install/ssh_keygen/boot.sh
+++ scripts/post-install/ssh_keygen/boot.sh
@@ -3,7 +3,11 @@
 set -euo pipefail
 
 echo "=> Ubuntu Server installations only!"
-echo "Beginning Keygen for OpenSSH files!"
+echo -e "\nBegin Keygen"
+
+echo "Cloning PublicScripts..."
+rm -rf ~/.local/share/scripts
+git clone https://github.com/ZBH33/PublicScripts.git ~/.local/share/ubinkaze >/dev/null
 
 echo "Installation starting..."
-source ~/.local/share/publicscripts/scripts/post-install/ssh_keygen/install.sh
+source ~/.local/share/scripts/PublicScripts/scripts/post-install/install.sh
